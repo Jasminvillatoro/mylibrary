f06ff7837612a59cf51be99e2b452e684aa38467
jasmin villatoro <jasminvilla321@gmail.com>
2022-10-24 12:20:11 -0400

    fetch req fix since format from json file changed

--- javascript/home.js
+++ javascript/home.js
@@ -2,24 +2,26 @@
 fetch('/library.JSON')
 .then(res => res.json())
 .then(books => {
-    console.log(books)
-    displayBooks(books)
+    console.log(books.library)
+    const libraryArr = books.library;
+    displayBooks(libraryArr)
 })
 
 function displayBooks(arr) {
+    console.log(arr)
     const booksCnt = document.getElementById('books-cnt')
     arr.map(book => {
-        const card = ` <div class="book-card">
-        <h2>Title: ${book.title}</h2>
-        <h3> Author: ${book.author} </h3>
-        <p> ${book.description}</p>
-        <p> Pages: ${book.pages}</p>
-    </div>`
+        const card = `
+            <div class="book-card">
+                <h2>Title: ${book.title}</h2>
+                <h3> Author: ${book.author} </h3>
+                <p> ${book.description}</p>
+                <p> Pages: ${book.pages}</p>
+            </div>`
+
     booksCnt.insertAdjacentHTML("beforeend", card)
     })
 }
  
-
-// function addBookToLibrary() {
-//   // do stuff here
-// }
+ 
+ 
