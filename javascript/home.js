792da8c30bc9ca5739ff798662ed550f94bca8c2
jasmin villatoro <jasminvilla321@gmail.com>
2022-10-24 15:57:57 -0400

    book cover

--- javascript/home.js
+++ javascript/home.js
@@ -13,6 +13,7 @@ function displayBooks(arr) {
     arr.map(book => {
         const card = `
             <div class="book-card">
+            <img class="book-cover" src="${book.imageUrl}" alt="book cover" width="120" height="200">
                 <h2>Title: ${book.title}</h2>
                 <h3> Author: ${book.author} </h3>
                 <p> ${book.description}</p>
